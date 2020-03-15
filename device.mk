a095aaa012a82738f589dd379b2d44f855bd1d6c
MD Raza <raza231198@gmail.com>
2020-03-15 14:25:01 +0530

    X00P: import power profile from stock
    Change-Id: I5a9add31dfd112841ccdbbf76b5c6317d96fbfe7

--- device.mk
+++ device.mk
@@ -9,5 +9,9 @@ DEVICE_PATH := device/asus/X00P
 # Inherit proprietary files
 $(call inherit-product, vendor/asus/X00P/X00P-vendor.mk)
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
+
 # Inherit from msm8937-common
 $(call inherit-product, device/asus/msm8937-common/msm8937.mk)
