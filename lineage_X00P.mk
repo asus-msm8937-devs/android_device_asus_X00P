11e1c35c8a8355c1bda9623c7264522fe56ce67c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-12-09 10:31:10 +0100

    X00P: Cleanup

--- lineage_X00P.mk
+++ lineage_X00P.mk
@@ -9,20 +9,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
-# Inherit from X00P device
-$(call inherit-product, device/asus/X00P/device.mk)
-
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit from X00P device
+$(call inherit-product, device/asus/X00P/device.mk)
+
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := X00P
-PRODUCT_NAME := lineage_X00P
 PRODUCT_BRAND := asus
-PRODUCT_MODEL := ASUS_X00PD
+PRODUCT_DEVICE := X00P
 PRODUCT_MANUFACTURER := asus
-TARGET_VENDOR := asus
-BOARD_VENDOR := asus
+PRODUCT_NAME := lineage_X00P
+PRODUCT_MODEL := ASUS_X00PD
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
@@ -31,5 +29,5 @@ VENDOR_SECURITY_PATCH := 2020-03-05
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="WW_X00PD-user 8.0.0 OPR1.170623.032 15.00.1801.31 release-keys"
 
-# Set BUILD_FINGERPRINT variable
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := asus/WW_X00PD/ASUS_X00P_2:8.0.0/OPR1.170623.032/15.00.1801.31:user/release-keys
