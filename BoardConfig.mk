bbcabe57eaed09eb5318a214b2011beb7f6e9df5
danascape <danascape@gmail.com>
2021-09-27 07:42:03 +0200

    X00P: Enable qcrild with SKU
    Change-Id: I19c0c02c3f22610c048c6df03e64f77c64bf1ed5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,5 +19,9 @@ BUILD_BROKEN_DUP_RULES := true
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := X00P_defconfig
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_X00P
+TARGET_RECOVERY_DEVICE_MODULES := libinit_X00P
+
 # Inherit the proprietary files
 -include vendor/asus/X00P/BoardConfigVendor.mk
