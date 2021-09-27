b441a416562a68abab739dcfcb6d3775febcdba8
danascape <danascape@gmail.com>
2021-09-27 08:49:58 +0200

    X00P: Label light HAL
    Change-Id: Ieb0b824b51dfc632d8e8180297d729baf893d7a0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,5 +29,8 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_X00P
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
+# SEPolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Inherit the proprietary files
 -include vendor/asus/X00P/BoardConfigVendor.mk
